4b30fd813e995b1af2e30d73c637c8e89700825b
cooluncule <vladislavmladenov03@gmail.com>
2026-05-30 19:37:33 +0300

    Initial scaffold for Vite+React resell-final

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+// src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ResellerPanelDashboard from "./pages/ResellerPanelDashboard";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ResellerPanelDashboard />
+  </React.StrictMode>
+);
